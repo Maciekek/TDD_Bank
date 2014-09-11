c24b11c132066ddd9014edadc6b5c9b2422fc0f8
Maciekek <maciekek@gmail.com>
2014-09-11 18:55:16 +0200

    create project with first pass test

---
+++ src/Dollar.java
@@ -0,0 +1,14 @@
+
+public class Dollar {
+	
+	int amount = 5*2;
+	
+	Dollar(int amount){
+		
+	}
+	
+	public void times(int multiplier){
+		
+	}
+
+}
---
+++ tests/BankTests.java
@@ -0,0 +1,16 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class BankTests {
+
+	@Test
+	public void testMultiplication() {
+		Dollar five = new Dollar(5);
+		five.times(2);
+		assertEquals(10,  five.amount);
+		
+	}
+
+}
